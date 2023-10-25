fcacba1d472b69b11e96a966c99ca4a09423f717
Dani Assa <dfassa1@gmail.com>
2023-10-25 14:19:08 -0300

    agregando json server

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,6 @@
 import navbar from "../Components/navbar.js";
+import footer1 from "../Components/footer.js";
 
 document.addEventListener('DOMContentLoaded', () => navbar ());
+
+document.addEventListener('DOMContentLoaded', () => footer1());
---
+++ utils/endpoints.js
@@ -0,0 +1,4 @@
+export const endpoints = {
+  users: 'http://localhost:3000/users',
+  products: 'http://localhost:3000/products'
+};
